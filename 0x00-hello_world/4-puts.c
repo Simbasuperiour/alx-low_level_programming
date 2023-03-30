9103d7d450d77f153471e8ff6d86d89aeb95287d
Immanuel Peter <immanuelpetersimba@gmail.com>
2023-03-30 04:22:38 -0700

    Literally finished all projects in hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints the phrase "programming is like building a multilingual puzzle"
+ * Return - 0 if exited properly and non zero if not.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
