cd2e138ba9e9fcad08f0be7bc74ea5a302da4712
Immanuel Peter <immanuelpetersimba@gmail.com>
2023-03-30 05:13:47 -0700

    project done

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,30 @@
+/*
+ * File: 5-string_toupper.c
+ * Auth: Immanuel Peter
+ */
+
+#include "main.h"
+
+/**
+ * string_toupper - Changes all lowercase letters
+ *                  of a string to uppercase.
+ * @str: The string to be changed.
+ *
+ * Return: A pointer to the changed string.
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+
+		i++;
+
+	}
+
+	return (str);
+
+}
